c20712ac2074f649b56b8fb3b5c02e5f8f40cd2c
Tobias Facen <tobiasfacen@gmail.com>
2021-05-22 20:51:10 -0300

    probamos la clase math y la clase string

--- src/App.java
+++ src/App.java
@@ -1,7 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
        
-        System.out.println("Hello, World!");
+        System.out.println("vamos a probar la clase math");
+        double base = 3;
+        double exponente = 5;
+        int resultado = (int)Math.pow(base, exponente);
+        System.out.print("el resultado de " + base + " elevado a la ");
+        System.out.println(exponente + " es igual a " + resultado);
+        System.out.println("probamos la clase String");
+        String nombre = "Tobias";
+        System.out.println("mi nombre es " + nombre);
+        System.out.println("mi nombre tiene " + nombre.length() + " letras.");
+        System.out.println("la primera letra de mi nombre es " + nombre.charAt(0));
+        int ultimaLetra = nombre.length();
+        System.out.println("y la ultima letra es " + nombre.charAt(ultimaLetra-1));
 
     }
 }
